65e9c7acbe0d371d17e0c1c1d986f8f226c92d2e
hailey_mueller <haileymueller12@gmail.com>
2021-05-04 03:24:51 +0000

    Made changes for Heroku capability

--- flaskApp.py
+++ flaskApp.py
@@ -8,4 +8,5 @@ def index():
     return "<h1>Hello World!</h1>"
 
 if __name__ == "__main__":
-    app.run(debug=True) #TODO: set debug=False before deployment!
+    port = os.environ.get("PORT", 5000) # Heroku will set the PORT environment variable for web traffic
+    app.run(debug=False, host="0.0.0.0", port=port) # TODO: set debug=False before deployment!
