46f128b97c3f6612bc5aaed1935a2cf2378f720b
hailey_mueller <haileymueller12@gmail.com>
2021-05-05 05:20:18 +0000

    Made small change to "port" code

--- flaskApp.py
+++ flaskApp.py
@@ -90,5 +90,5 @@ def predict_well(instance):
         return None
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000)) # Heroku will set the PORT environment variable for web traffic
+    port = os.environ.get("PORT", 5000) # Heroku will set the PORT environment variable for web traffic
     app.run(debug=False, host="0.0.0.0", port=port) # TODO: set debug=False before deployment!
